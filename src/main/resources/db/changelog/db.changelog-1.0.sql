452b37003ffaf4b4c13ae39a0a6b1846adf744e1
Artem Pupyshev <hdboomone.art@gmail.com>
2025-03-01 01:20:40 +0300

    yaml files added

--- src/main/resources/db/changelog/db.changelog-1.0.sql
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -1,31 +1,26 @@
---changeSet Artem_Pupyshev:1
 --liquibase formatted sql
-CREATE TABLE loan_type(
-                          id serial primary key,
-                          Name varchar(50) NOT NULL UNIQUE
+
+--changeset Artem_Pupyshev:1 runOnChange:false
+CREATE TABLE loan_type (
+                           id SERIAL PRIMARY KEY,
+                           Name VARCHAR(50) NOT NULL UNIQUE
 );
 
-insert into loan_type(Name)
-values('Sberpank'),
-      ('TZ-bank');
+INSERT INTO loan_type (Name)
+VALUES ('Sberpank'),
+       ('TZ-bank');
 
-
-
-
-CREATE table students_loans(
-                               id serial PRIMARY KEY,
-                               First_Name varchar(50) NOT NULL,
-                               Second_Name varchar(50) NOT NULL ,
-                               Loan_Type int references Loan_Type(id),
-                               Loan_Amount NUMERIC(10,4) NOT NULL
+CREATE TABLE students_loans (
+                                id SERIAL PRIMARY KEY,
+                                First_Name VARCHAR(50) NOT NULL,
+                                Second_Name VARCHAR(50) NOT NULL,
+                                Loan_Type INT REFERENCES loan_type(id),
+                                Loan_Amount NUMERIC(10,4) NOT NULL
 );
 
-insert into students_loans(First_Name,Second_Name,Loan_Type,Loan_Amount)
-values ('Artem','Kokos',1,500.2),
+INSERT INTO students_loans (First_Name, Second_Name, Loan_Type, Loan_Amount)
+VALUES ('Artem','Kokos',1,500.2),
        ('Bob','Bobov',2,50000.2);
 
 --rollback DROP TABLE students_loans;
 --rollback DROP TABLE loan_type;
-
---tag v1.0
-
