745124c91a48912444f85609e8523a1b8b4f8b3b
Artem Pupyshev <hdboomone.art@gmail.com>
2025-02-28 23:54:13 +0300

    liquibase.properties added

--- src/main/resources/db/changelog/db.changelog-1.0.sql
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -1,18 +1,18 @@
 --changeSet Artem_Pupyshev:1
 --liquibase formatted sql
-CREATE TABLE Loan_Type(
+CREATE TABLE loan_type(
                           id serial primary key,
                           Name varchar(50) NOT NULL UNIQUE
 );
 
-insert into Loan_Type(Name)
+insert into loan_type(Name)
 values('Sberpank'),
       ('TZ-bank');
 
 
 
 
-CREATE table Students_Loans(
+CREATE table students_loans(
                                id serial PRIMARY KEY,
                                First_Name varchar(50) NOT NULL,
                                Second_Name varchar(50) NOT NULL ,
@@ -20,7 +20,12 @@ CREATE table Students_Loans(
                                Loan_Amount NUMERIC(10,4) NOT NULL
 );
 
-insert into Students_Loans(First_Name,Second_Name,Loan_Type,Loan_Amount)
+insert into students_loans(First_Name,Second_Name,Loan_Type,Loan_Amount)
 values ('Artem','Kokos',1,500.2),
        ('Bob','Bobov',2,50000.2);
+
+--rollback DROP TABLE students_loans;
+--rollback DROP TABLE loan_type;
+
+--tag v1.0
 
