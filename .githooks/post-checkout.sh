8e2e989a3880075368099fd824ddf421cdc674b2
Artem Pupyshev <hdboomone.art@gmail.com>
2025-03-24 00:05:38 +0300

    test 6

--- .githooks/post-checkout.sh
+++ .githooks/post-checkout.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
-# Переходим из .githooks в корень репозитория (ОДИН уровень)
-cd "$(dirname "$0")/.."
+  #!/bin/sh
+  # Переходим из .githooks в корень репозитория (ОДИН уровень)
+  cd "$(dirname "$0")/.."
 
-FILE_PATH="src/main/resources/db/changelog/db.changelog-master.yaml"
-FILE_CONTENT=$(git show "$2:$FILE_PATH")
+  FILE_PATH="src/main/resources/db/changelog/db.changelog-master.yaml"
+  FILE_CONTENT=$(git show "$2:$FILE_PATH")
 
-LAST_LINE=$(echo "$FILE_CONTENT" | grep 'file:' | tail -n 1)
-VERSION=$(echo "$LAST_LINE" | sed 's/^.*db\.changelog-\(.*\)\.yml/\1/')
+  LAST_LINE=$(echo "$FILE_CONTENT" | grep 'file:' | tail -n 1)
+  VERSION=$(echo "$LAST_LINE" | sed 's/^.*db\.changelog-\(.*\)\.yml/\1/')
 
-# Если новый коммит ($2) является предком старого ($1), значит откат (rollback)
-if git merge-base --is-ancestor "$2" "$1"; then
-  liquibase --defaultsFile="liquibase.properties" rollback v$VERSION
-else
-  liquibase --defaultsFile="liquibase.properties" update
-fi
+  # Если новый коммит ($2) является предком старого ($1), значит откат (rollback)
+  if git merge-base --is-ancestor "$2" "$1"; then
+    liquibase --defaultsFile="liquibase.properties" rollback v$VERSION
+  else
+    liquibase --defaultsFile="liquibase.properties" update
+  fi
 
-exit 0
+  exit 0
