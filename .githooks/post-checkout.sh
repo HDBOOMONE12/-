58b58a52e50678c737751ba598095cf410100f91
Artem Pupyshev <hdboomone.art@gmail.com>
2025-03-23 22:50:18 +0300

    post-checkout.sh Добавлен поиск бета поиск тега

--- .githooks/post-checkout.sh
+++ .githooks/post-checkout.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 cd "$(dirname "$0")/.."
 FILE_PATH="src/main/resources/db/changelog/db.changelog-master.yaml"
-
-# Отладочный вывод параметров
-echo "Post-checkout hook triggered with parameters: $1, $2, $3" >> /tmp/post-checkout.log
-
-# Убираем проверку $3, чтобы выполнялось всегда
-LAST_LINE=$(grep 'file:' "$FILE_PATH" | tail -n 1)
+FILE_CONTENT=$(git show "$2:$FILE_PATH")
+LAST_LINE=$(echo "$FILE_CONTENT" | grep 'file:' | tail -n 1)
 VERSION=$(echo "$LAST_LINE" | sed 's/^.*db\.changelog-\(.*\)\.yml/\1/')
-if [ -n "$VERSION" ]; then
-  echo "$VERSION"
-else
-  echo "Версия не найдена"
-fi
-
+echo "$VERSION"
 exit 0
